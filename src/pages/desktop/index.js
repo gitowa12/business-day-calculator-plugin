9a9f2386bde2afd265dac73e58fdad9994c67ae7
gitowa12 <118157881+gitowa12@users.noreply.github.com>
2024-02-20 12:59:02 +0900

    feat: desktop.jsで営業日情報を取得

--- src/pages/desktop/index.js
+++ src/pages/desktop/index.js
@@ -1,111 +1,51 @@
-import "./index.css";
+// import "./index.css";
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+import { GetConfig } from "../../services/GetConfig";
 
-(function (PLUGIN_ID) {
-  const holidayAppId = 416;
-  let holidays = [];
-  const user = kintone.getLoginUser().code;
-  // 実行ユーザー
-  const users = [
-    "Administrator",
-    "masuda.daichi@jp.panasonic.com",
-    "hosoda.eriko@kk.jp.panasonic.com",
-    "nakao.kaori001@kk.jp.panasonic.com",
-    "towa.sunagawa@persol-pt.co.jp",
-    "mineo.yoshikawa@persol-pt.co.jp",
-  ];
-  // レコード全件取得API
-  const recordGetAll = async function (param, errmsg) {
-    const client = new KintoneRestAPIClient();
-    const params = {};
-    if (param.app) {
-      params.app = param.app;
-    } else {
-      errmsg.message.push("取得対象のアプリIDが設定されていません。");
-      return false;
-    }
-    if (param.fields) {
-      params.fields = param.fields;
-    }
-    if (param.condition) {
-      params.condition = param.condition;
-    }
-    if (param.orderBy) {
-      params.orderBy = param.orderBy;
-    }
-    if (param.withCursor) {
-      params.withCursor = param.withCursor;
-    }
-    let resp = await client.record
-      .getAllRecords(params)
-      .catch(async function (err) {
-        console.log(err);
-        errmsg.message.push("レコードの取得に失敗しました。");
-        return false;
-      });
-    if (resp) {
-      return resp;
-    } else {
-      return false;
-    }
+((PLUGIN_ID) => {
+  const PLUGIN_ID = kintone.$PLUGIN_ID;
+  console.log("PLUGIN_ID", PLUGIN_ID);
+  const client = new KintoneRestAPIClient();
+
+  const holidayAppParam = {
+    app: 9,
+    fields: ["祝日"],
   };
+
+  // // 実行ユーザー指定
+  // const user = kintone.getLoginUser().code;
+  // const users = ["Administrator", "towadev12@gmail.com"];
+
   // 祝祭日取得
-  const getHoliday = async (errmsg) => {
-    const param = {
-      app: holidayAppId,
-      fields: ["祝日"],
-    };
-    let resp = await recordGetAll(param).catch(async function (err) {
-      console.log(err);
-      errmsg.message.push("レコードの取得処理に失敗しました。");
-      return false;
-    });
-    if (resp) {
-      holidays = await resp.map(function (record) {
+  const getHoliday = async (param) => {
+    try {
+      const resp = await client.record.getAllRecords(param);
+      const holidays = resp.map((record) => {
         return record.祝日.value;
       });
-      console.log(holidays);
-      return true;
-    } else {
+      console.log("holidays", holidays);
+      return holidays;
+    } catch (error) {
+      console.log(error);
+      console.log("レコードの取得に失敗しました。");
       return false;
     }
   };
 
-  if (users.includes(user)) {
-    kintone.events.on(
-      ["app.record.create.show", "app.record.edit.show"],
-      async function (event) {
-        const errmsg = {
-          message: [],
-        };
-        let resp = await getHoliday(errmsg).catch(async function (err) {
-          console.log(err);
-          errmsg.message.push("祝祭日取得処理に失敗しました。");
-          return false;
-        });
-      }
-    );
-    const events = ["app.record.create.submit", "app.record.edit.submit"];
-    kintone.events.on(events, async function (event) {
-      const eventType = event.type;
-      console.log(event);
-      let record = event.record;
-      console.log(record);
-      console.log(event);
-      /*if(hogehoge){
-        let resp = window.confirm('hogehoge');
-        if(!resp){
-          return false;
+  // if (users.includes(user)) {}
+  kintone.events.on(
+    ["app.record.create.show", "app.record.edit.show"],
+    async (event) => {
+      const holidays = getHoliday(holidayAppParam);
+      const config = GetConfig();
+      console.log("config", config);
+      kintone.events.on(
+        ["app.record.create.submit", "app.record.edit.submit"],
+        async (event) => {
+          return event;
         }
-      }*/
+      );
       return event;
-    });
-  }
-  //前回の設定を取得する関数
-
-  //祝祭日カレンダーアプリから、祝祭日情報を取得する関数
-
-  //営業日計算関数
-
-  // kintone.events
+    }
+  );
 })(kintone.$PLUGIN_ID);
--- src/services/GetConfig.tsx
+++ src/services/GetConfig.tsx
@@ -2,6 +2,7 @@ export const GetConfig = (): any => {
   // 前回の設定の取得とデータの整形
   // console.log(kintone.$PLUGIN_ID);
   const PLUGIN_ID = kintone.$PLUGIN_ID;
+  console.log("PLUGIN_ID", PLUGIN_ID);
   const getConfig = kintone.plugin.app.getConfig(PLUGIN_ID);
   // console.log("生データ", getConfig);
 
